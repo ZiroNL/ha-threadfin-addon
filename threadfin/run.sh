ee85313f15d54857c22b39fe4eba83e03e587f26
ZiroNL <mark@ziro.nl>
2026-02-06 21:15:50 +0000

    fix 3

--- threadfin/run.sh
+++ threadfin/run.sh
@@ -2,8 +2,10 @@
 
 CONFIG_PATH="/config/threadfin"
 
+# Zorg dat config folder bestaat
 mkdir -p "$CONFIG_PATH"
 
+# Start threadfin
 exec /opt/threadfin/threadfin \
     -port=34400 \
     -config="$CONFIG_PATH"
